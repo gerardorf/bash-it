081cbd722272d2d2f47562b777f54c10b76b3e99
gerardorf <gerardorf@gmail.com>
2015-12-15 15:15:55 +0100

    add fuck command

---
+++ custom/fuck.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# enable fuck
+
+eval "$(thefuck --alias)"
+# You can use whatever you want as an alias, like for Mondays:
+eval "$(thefuck --alias FUCK)"
+# in spanish:
+eval "$(thefuck --alias caguen)"
