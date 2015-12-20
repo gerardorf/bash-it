a1926547b4cc0e0fe023aec797b0336c67a22e6d
gerardorf <gerardorf@gmail.com>
2015-12-20 11:33:48 +0100

    add autocorrections

---
+++ custom/autocorrect-typos.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# No need to use cd anymore
+shopt -s autocd
+
+# Autocorrects minor errors on directories names
+shopt -s cdspell
+
+# Case-insensitive filename autocompletion using TAB
+shopt -s nocaseglob
