23ea5d9202163b21de9f7741063f94059b77cf35
gerardorf <gerardorf@gmail.com>
2015-12-24 13:39:06 +0100

    fix home location

--- custom/closure-compiler.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# set CLOSURE_COMPILER and CLOSURE_LIBRARY environment variables
-#
-
-export CLOSURE_COMPILER=/opt/closure-compiler/
-export CLOSURE_LIBRARY=/opt/closure-library/
--- custom/nvm.bash
+++ custom/nvm.bash
@@ -2,6 +2,6 @@
 #
 # Loads nvm
 
-export NVM_DIR="/home/gerardorf/.nvm"
+export NVM_DIR=~/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use 0.12
